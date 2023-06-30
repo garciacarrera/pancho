f54baf9726d577a8338c1c2a7fd48d55fbf11102
garciacarrera <fxtr088@gmail.com>
2023-06-30 01:15:20 -0300

    creacion de un componente item

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import {  Item } from "../../models/items";
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent {
 
+export class ItemsComponent {
+items: Item[]=[];
+constructor(){}
+ngOnInit(): void{
+ this.items=[
+  {
+    id: 0,
+    title: 'pancho ',
+    price: 550,
+    quantity:1,
+    completed: false
+  },
+  {
+    id: 1,
+    title: 'pancho doble',
+    price: 1000,
+    quantity:1,
+    completed: true 
+  },
+ ];
+}
 }
